cdc5148f956406cd6f054ed45f3663fdb0fdacb9
Kristján Oddsson <koddsson@gmail.com>
2022-05-11 22:30:39 +0100

    meta-viewport rule

---
+++ src/rules/meta-viewport.ts
@@ -0,0 +1,38 @@
+const text = 'Zooming and scaling must not be disabled'
+const url = 'https://dequeuniversity.com/rules/axe/4.4/meta-viewport?application=RuleDescription'
+
+function parseContent(content: string): Record<string, string> {
+  const obj: Record<string, string> = {}
+  for (const pair of content.split(',')) {
+    const [key, value] = pair.split('=')
+    obj[key.toLowerCase()] = value.toLowerCase()
+  }
+  return obj
+}
+
+export default function metaViewport(el: Element) {
+  const errors = []
+
+  // TODO: Do the same for all the other rules
+  const selector = 'meta[name=viewport]'
+  const elements = Array.from(el.querySelectorAll<HTMLMetaElement>(selector))
+  if (el.matches(selector)) elements.push(el as HTMLMetaElement)
+  for (const element of elements) {
+    const content = parseContent(element.content)
+    if (content['user-scalable'] === 'no' || content['user-scalable'] === '0') {
+      errors.push({
+        element,
+        text,
+        url,
+      })   
+    }
+    if (parseFloat(content['maximum-scale']) < 2) {
+      errors.push({
+        element,
+        text,
+        url,
+      })   
+    }
+  }
+  return errors
+}
---
+++ tests/meta-viewport.ts
@@ -0,0 +1,68 @@
+import { fixture, html, expect } from "@open-wc/testing";
+import { scan } from "../../../../src/scanner";
+
+describe("meta-viewport", function () {
+  it("maximum-scale=2 has no errors", async () => {
+    const meta = await fixture(html`
+      <div>
+      <meta name="viewport" id="mvp" content="maximum-scale=2" />
+      </div>
+    `);
+
+    const results = (await scan(meta)).map(({ text, url }) => {
+      return { text, url };
+    });
+
+    expect(results).to.be.empty;
+  });
+
+  it("initial-scale is fine", async () => {
+    const meta = await fixture(html`
+      <meta
+        name="viewport"
+        id="mvp"
+        content="width=device-width, initial-scale=1.0"
+      />
+    `);
+
+    expect(await scan(meta)).to.be.empty;
+  });
+
+  it("user-scalable should fail", async () => {
+    const meta = await fixture(html`
+      <meta
+        name="viewport"
+        id="mvp"
+        content="User-Scalable=no, maximum-scale=2"
+      />
+    `);
+
+    const results = (await scan(meta)).map(({ text, url }) => {
+      return { text, url };
+    });
+
+    expect(results).to.eql([
+      {
+        text: "Zooming and scaling must not be disabled",
+        url: "https://dequeuniversity.com/rules/axe/4.4/meta-viewport?application=RuleDescription",
+      },
+    ]);
+  });
+
+  it("maximum-scale less than 2 should error", async () => {
+    const meta = await fixture(html`
+      <meta name="viewport" id="mvp" content="maximum-scale=1.5;" />
+    `);
+
+    const results = (await scan(meta)).map(({ text, url }) => {
+      return { text, url };
+    });
+
+    expect(results).to.eql([
+      {
+        text: "Zooming and scaling must not be disabled",
+        url: "https://dequeuniversity.com/rules/axe/4.4/meta-viewport?application=RuleDescription",
+      },
+    ]);
+  });
+});
