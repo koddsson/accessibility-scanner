31ced288085b4febcb66d36a0283546ad46ca6a5
Kristján Oddsson <koddsson@gmail.com>
2022-05-11 22:27:19 +0100

    More testing framework work

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,6 +1,7 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
 export default {
-  files: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
+  nodeResolve: true,
+  files: ['tests/**/*.ts'],
   plugins: [esbuildPlugin({ ts: true })],
 };
