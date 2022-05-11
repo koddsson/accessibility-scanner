6fbf8ae168f0a06dd95033b3a08b86edd6ad6ac7
Kristján Oddsson <koddsson@gmail.com>
2022-05-11 22:33:30 +0100

    Remove node_modules

--- node_modules/@web/dev-server/dist/plugins/nodeResolvePlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import { RollupNodeResolveOptions } from '@web/dev-server-rollup';
-import { Plugin } from '@web/dev-server-core';
-export declare function nodeResolvePlugin(rootDir: string, preserveSymlinks?: boolean, userOptions?: RollupNodeResolveOptions): Plugin;
-//# sourceMappingURL=nodeResolvePlugin.d.ts.map
