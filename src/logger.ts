aeedffa16f3a8ee2670cb329c2fb220e98832e01
Kristján Oddsson <koddsson@gmail.com>
2022-05-20 21:45:23 +0100

    Try out a scheduling scanner

---
+++ src/logger.ts
@@ -0,0 +1,28 @@
+enum LogLevel {
+  OFF = 0,
+  ERROR = 1,
+  WARN = 2,
+  INFO = 3,
+  DEBUG = 4,
+  TRACE = 5,
+}
+
+export class Logger {
+  #loglevel: LogLevel
+
+  constructor(loglevel: LogLevel = LogLevel.DEBUG) {
+    this.#loglevel = loglevel
+  }
+
+  log(...data: unknown[]) {
+    const userSetting: LogLevel = Number(
+      localStorage.getItem("@koddsson/accessibility-runner:loglevel")
+    );
+    if (userSetting === LogLevel.OFF) return;
+    if (userSetting >= this.#loglevel) {
+      const key = LogLevel[this.#loglevel];
+      // @ts-ignore
+      console[key](data);
+    }
+  }
+}
--- src/scanner.ts
+++ src/scanner.ts
@@ -1,26 +1,30 @@
 export interface AccessibilityError {
-  text: string
-  url: string
-  element: Element
+  text: string;
+  url: string;
+  element: Element;
 }
 
-import {areaAlt} from './rules/area-alt'
+import { areaAlt } from "./rules/area-alt";
 //import {ariaAllowedAttr} from './rules/aria-allowed-attr'
-import {ariaHiddenBody} from './rules/aria-hidden-body'
-import ariaRequiredAttr from './rules/aria-required-attr'
+import { ariaHiddenBody } from "./rules/aria-hidden-body";
+import ariaRequiredAttr from "./rules/aria-required-attr";
 //import ariaRoles from './rules/aria-roles'
-import metaViewport from './rules/meta-viewport'
-import scopeAttrValid from './rules/scope-attr-valid'
-import videoCaptions from './rules/video-caption'
-import selectName from './rules/select-name'
-import metaRefresh from './rules/meta-refresh'
-import imageAlt from './rules/image-alt'
-import buttonName from './rules/button-name'
-import label from './rules/label'
+import metaViewport from "./rules/meta-viewport";
+import scopeAttrValid from "./rules/scope-attr-valid";
+import videoCaptions from "./rules/video-caption";
+import selectName from "./rules/select-name";
+import metaRefresh from "./rules/meta-refresh";
+import imageAlt from "./rules/image-alt";
+import buttonName from "./rules/button-name";
+import label from "./rules/label";
 
-type Rule = (el: Element) => AccessibilityError[]
+import {Logger} from './logger'
 
-export const allRules: Rule[]  = [
+const logger = new Logger()
+
+type Rule = (el: Element) => AccessibilityError[];
+
+export const allRules: Rule[] = [
   areaAlt,
   //ariaAllowedAttr,
   ariaHiddenBody,
@@ -33,23 +37,58 @@ export const allRules: Rule[]  = [
   metaRefresh,
   imageAlt,
   buttonName,
-  label
-]
+  label,
+];
 
-export async function scan(element: Element, enabledRules?: Rule[]): Promise<AccessibilityError[]> {
-  const errors: AccessibilityError[] = []
-  const rules = enabledRules || allRules
+export async function requestIdleScan(
+  element: Element,
+  enabledRules: Rule[]
+): Promise<AccessibilityError[]> {
+  const errors: AccessibilityError[] = [];
+  const rules = enabledRules || allRules;
+
+  return new Promise((resolve) => {
+    requestIdleCallback(async function executeScan(deadline: IdleDeadline) {
+      while (
+        (deadline.timeRemaining() > 0 || deadline.didTimeout) &&
+        rules.length
+      ) {
+        logger.log(deadline.timeRemaining(), deadline.didTimeout)
+        const rule = rules.shift()!;
+        logger.log(`Executing ${rule.name}`)
+        errors.push(...rule(element));
+      }
+
+      if (rules.length) {
+        console.log(`exited with ${allRules.length} left`)
+        requestIdleCallback(executeScan);
+      } else {
+        resolve(errors);
+      }
+    });
+  });
+}
+
+export async function scan(
+  element: Element,
+  enabledRules?: Rule[]
+): Promise<AccessibilityError[]> {
+  const errors: AccessibilityError[] = [];
+  const rules = enabledRules || allRules;
   for (const rule of rules) {
-    errors.push(...rule(element))
+    errors.push(...rule(element));
   }
 
-  return errors
+  return errors;
 }
 
 export class Scanner {
   constructor(public enabledRules?: Rule[]) {}
 
   async scan(element?: Element): Promise<AccessibilityError[]> {
-    return scan(element ?? document.documentElement, this.enabledRules ?? allRules) 
+    return scan(
+      element ?? document.documentElement,
+      this.enabledRules ?? allRules
+    );
   }
 }
