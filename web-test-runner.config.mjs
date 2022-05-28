b52a254de0165c3499ee72822a491a1d7316f506
Kristján Oddsson <koddsson@gmail.com>
2022-05-28 21:48:32 +0100

    Run tests in Chromium, Firefox and Safari

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,7 +1,13 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default {
   nodeResolve: true,
   files: ['tests/**/*.ts'],
   plugins: [esbuildPlugin({ ts: true, target: 'esnext' })],
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' }),
+  ],
 };
