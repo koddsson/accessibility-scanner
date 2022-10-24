6c2e4d6b81563863ce3340068f7b209c0eb9f47c
Kristján Oddsson <koddsson@gmail.com>
2022-10-24 09:53:10 +0100

    Start running the ACT testcases suite against the scanner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,21 +1,19 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { playwrightLauncher } from '@web/test-runner-playwright';
+import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { playwrightLauncher } from "@web/test-runner-playwright";
 
-const browsers = [
-  playwrightLauncher({ product: 'chromium' }),
-]
+const browsers = [playwrightLauncher({ product: "chromium" })];
 
 if (process.env.CI) {
   browsers.push(
-    playwrightLauncher({ product: 'firefox' }),
-    playwrightLauncher({ product: 'webkit' }),
-  )
+    playwrightLauncher({ product: "firefox" }),
+    playwrightLauncher({ product: "webkit" })
+  );
 }
 
 export default {
   nodeResolve: true,
   coverage: true,
-  files: ['tests/**/*.ts'],
-  plugins: [esbuildPlugin({ ts: true, target: 'esnext' })],
+  files: ["tests/**/*.ts"],
+  plugins: [esbuildPlugin({ ts: true, target: "esnext" })],
   browsers,
 };
