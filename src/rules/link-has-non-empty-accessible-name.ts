b4d091cf1998b01b464a808fa20751b1a60a689f
Kristján Oddsson <koddsson@gmail.com>
2022-10-25 19:12:27 +0100

    Run prettier

--- src/rules/link-has-non-empty-accessible-name.ts
+++ src/rules/link-has-non-empty-accessible-name.ts
@@ -1,8 +1,8 @@
 import { AccessibilityError } from "../scanner";
-import {isVisible, labelledByIsValid} from '../utils'
+import { isVisible, labelledByIsValid } from "../utils";
 
-const id = 'c487ae'
-const url = `https://act-rules.github.io/rules/${id}`
+const id = "c487ae";
+const url = `https://act-rules.github.io/rules/${id}`;
 const text = "This rule checks that each link has a non-empty accessible name.";
 
 export default function (el: Element): AccessibilityError[] {
@@ -13,18 +13,18 @@ export default function (el: Element): AccessibilityError[] {
   }
   for (const element of elements) {
     if (!isVisible(element)) continue;
-    if (element.hasAttribute('title')) continue
-    const textContent = element.textContent?.trim()
-    if (textContent === '') {
-      const image = element.querySelector('img, svg')
-      const alt = image && image.getAttribute('alt')
-      if (alt) continue
-      const ariaLabel = image && image.getAttribute('aria-label')
-      if (ariaLabel) continue
-      const title = image && image.getAttribute('title')
-      if (title) continue
+    if (element.hasAttribute("title")) continue;
+    const textContent = element.textContent?.trim();
+    if (textContent === "") {
+      const image = element.querySelector("img, svg");
+      const alt = image && image.getAttribute("alt");
+      if (alt) continue;
+      const ariaLabel = image && image.getAttribute("aria-label");
+      if (ariaLabel) continue;
+      const title = image && image.getAttribute("title");
+      if (title) continue;
 
-      if (image && labelledByIsValid(image)) continue
+      if (image && labelledByIsValid(image)) continue;
 
       errors.push({
         element,
