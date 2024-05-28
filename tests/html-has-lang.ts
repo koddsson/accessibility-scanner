9d81dcfbbd6c509e00a629db6ddf632985db8d8c
Kristján Oddsson <koddsson@gmail.com>
2024-05-28 15:59:35 +0200

    add more testcases and implement rule better
    Co-authored-by: Michiel Pauw <pauw.michiel@gmail.com>
    Co-authored-by: NioniosSpeed <den.auth@gmail.com>

--- tests/html-has-lang.ts
+++ tests/html-has-lang.ts
@@ -1,10 +1,76 @@
-import { expect } from "@open-wc/testing";
+import { expect, fixture, html, nextFrame } from "@open-wc/testing";
 import { scan } from "../src/scanner";
 import htmlHasLang from "../src/rules/html-has-lang";
 
+/**
+ * Create a `<html></html>` in a iframe so that we aren't using the `<html>` element of the test runner.
+ */
+async function createHTMLElement(htmlString: string): Promise<HTMLElement> {
+  return (
+    await fixture(html`<iframe srcdoc="<!DOCTYPE html>${htmlString}"></iframe>`)
+  ).contentDocument // @ts-expect-error Just trust me bro
+    .querySelector("html")!;
+}
+
 describe("html-has-lang", function () {
-  it("empty alt attribute fails", async () => {
-    const container = document.createElement("html");
+  it("html element without a lang attribute fails", async () => {
+    const container = await createHTMLElement("<html></html>");
+
+    // Not sure why there's a timing issue here but this seems to fix it
+    await nextFrame();
+
+    const results = (await scan(container, [htmlHasLang])).map(
+      ({ text, url }) => {
+        return { text, url };
+      },
+    );
+
+    expect(results).to.eql([
+      {
+        text: "<html> element must have a lang attribute",
+        url: "https://dequeuniversity.com/rules/axe/4.4/html-has-lang",
+      },
+    ]);
+  });
+
+  it("html element with a empty lang attribute fails", async () => {
+    const container = await createHTMLElement('<html lang=""></html>');
+
+    const results = (await scan(container, [htmlHasLang])).map(
+      ({ text, url }) => {
+        return { text, url };
+      },
+    );
+
+    expect(results).to.eql([
+      {
+        text: "<html> element must have a lang attribute",
+        url: "https://dequeuniversity.com/rules/axe/4.4/html-has-lang",
+      },
+    ]);
+  });
+
+  it("html element with a boolean lang attribute fails", async () => {
+    const container = await createHTMLElement("<html lang></html>");
+
+    const results = (await scan(container, [htmlHasLang])).map(
+      ({ text, url }) => {
+        return { text, url };
+      },
+    );
+
+    expect(results).to.eql([
+      {
+        text: "<html> element must have a lang attribute",
+        url: "https://dequeuniversity.com/rules/axe/4.4/html-has-lang",
+      },
+    ]);
+  });
+
+  it("html element with a whitespace only lang attribute fails", async () => {
+    const container = await createHTMLElement(
+      '<html lang="                 "></html>',
+    );
 
     const results = (await scan(container, [htmlHasLang])).map(
       ({ text, url }) => {
