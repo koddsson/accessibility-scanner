d994d624355243776977532e009c01e64c4fa37a
Kristján Oddsson <koddsson@gmail.com>
2024-06-13 15:17:49 +0200

    update script

--- generate-readme.ts
+++ generate-readme.ts
@@ -1,17 +1,28 @@
 import { writeFile, readFile } from "node:fs/promises";
 import tablemark from "tablemark";
 
-const rules = JSON.parse(await readFile("./rules.json", "utf8"));
+const sections: Record<
+  string,
+  { description: string; rules: Array<Record<string, unknown>> }
+> = JSON.parse(await readFile("./rules.json", "utf8"));
+const tables = [];
 
-const table = tablemark(rules);
+for (const [name, { description, rules }] of Object.entries(sections)) {
+  const table = tablemark(rules);
+  tables.push({
+    name,
+    description,
+    table,
+  });
+}
 
 const document = `
 # accessbility-scanner
 
 ## AXE Rules
 
-### WCAG 2.0 Level A & AA Rules
-
-${table}`.trim();
+${tables.map(({ name, description, table }) => {
+  return `### ${name}\n\n${description}\n\n${table}\n\n`;
+})}`.trim();
 
 await writeFile("./README.md", document, "utf8");
