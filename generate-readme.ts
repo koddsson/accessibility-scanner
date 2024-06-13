1c0a0ed6c160c3f91d4e28f3d71b9b92a2ca8969
Kristján Oddsson <koddsson@gmail.com>
2024-06-13 15:36:24 +0200

    generate README from data (#30)
    * generate README from data
    
    * update script
    
    * fix newlines
    
    * regenerate README

---
+++ generate-readme.ts
@@ -0,0 +1,30 @@
+import { writeFile, readFile } from "node:fs/promises";
+import tablemark from "tablemark";
+
+const sections: Record<
+  string,
+  { description: string; rules: Array<Record<string, unknown>> }
+> = JSON.parse(await readFile("./rules.json", "utf8"));
+const tables = [];
+
+for (const [name, { description, rules }] of Object.entries(sections)) {
+  const table = tablemark(rules);
+  tables.push({
+    name,
+    description,
+    table,
+  });
+}
+
+const document = `
+# accessbility-scanner
+
+## AXE Rules
+
+${tables
+  .map(({ name, description, table }) => {
+    return `### ${name}\n\n${description}\n\n${table}\n`;
+  })
+  .join("\n")}`.trim();
+
+await writeFile("./README.md", document, "utf8");
