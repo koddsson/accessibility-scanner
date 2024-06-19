fae85ac7d9d1a3d454ccb215f1cc00338b708e86
Kristján Oddsson <koddsson@gmail.com>
2024-06-19 22:16:18 +0200

    aria-required-attr (#37)
    * aria-required-attr
    
    * try fixing tooltip tests
    
    * use summary reporter
    
    * publish test results
    
    * increase permissions for action

---
+++ src/rules/aria-required-attr.ts
@@ -0,0 +1,71 @@
+import { AccessibilityError } from "../scanner";
+import { querySelectorAll } from "../utils";
+
+// TODO: This list is incomplete!
+type Role =
+  | "checkbox"
+  | "combobox"
+  | "heading"
+  | "menuitemcheckbox"
+  | "menuitemradio"
+  | "meter"
+  | "radio"
+  | "scrollbar"
+  | "seperator"
+  | "slider"
+  | "switch";
+
+// TODO: This list is incomplete!
+type AriaAttribute =
+  | "aria-checked"
+  | "aria-expanded"
+  | "aria-level"
+  | "aria-checked"
+  | "aria-valuenow"
+  | "aria-controls";
+
+/**
+ * Required States and Properties:
+ *
+ * @see https://w3c.github.io/aria/#authorErrorDefaultValuesTable
+ */
+const roleToRequiredStatesAndPropertiesMaps: Record<Role, AriaAttribute[]> = {
+  checkbox: ["aria-checked"],
+  combobox: ["aria-expanded"],
+  heading: ["aria-level"],
+  menuitemcheckbox: ["aria-checked"],
+  menuitemradio: ["aria-checked"],
+  meter: ["aria-valuenow"],
+  radio: ["aria-checked"],
+  scrollbar: ["aria-controls", "aria-valuenow"],
+  // If focusable
+  seperator: ["aria-valuenow"],
+  slider: ["aria-valuenow"],
+  switch: ["aria-checked"],
+};
+
+const id = "aria-required-attr";
+const text = "Required ARIA attributes must be provided";
+const url = `https://dequeuniversity.com/rules/axe/4.4/${id}?application=RuleDescription`;
+
+export function ariaRequiredAttr(el: Element): AccessibilityError[] {
+  const errors = [];
+
+  const selector = Object.entries(roleToRequiredStatesAndPropertiesMaps)
+    .map(([role, attributes]) => {
+      return `[role=${role}]:not(${attributes.map((attr) => `[${attr}]`).join("")})`;
+    })
+    .join(",");
+
+  const elements = querySelectorAll(selector, el);
+  if (el.matches(selector)) elements.push(el);
+
+  for (const element of elements) {
+    errors.push({
+      element,
+      text,
+      url,
+    });
+  }
+  return errors;
+}
