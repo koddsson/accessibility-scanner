730ff4a0e47d43a6cfdf3ecfd8862ce1e05a3fd6
Kristján Oddsson <koddsson@gmail.com>
2024-06-26 10:40:01 +0200

    fix bad merge

--- tests/aria-tooltip-name.ts
+++ tests/aria-tooltip-name.ts
@@ -1,62 +1,3 @@
-import { fixture, html, expect } from "@open-wc/testing";
-import { Scanner } from "../src/scanner";
-import { ariaTooltipName } from "../src/rules/aria-tooltip-name";
-
-const scanner = new Scanner([ariaTooltipName]);
-
-const passes = [
-  `<div role="tooltip" id="al" aria-label="Name"></div>`,
-  `<div>
-    <div role="tooltip" id="alb" aria-labelledby="labeldiv"></div>
-    <div id="labeldiv">Hello world!</div>
-  </div>`,
-  `<div role="tooltip" id="combo" aria-label="Aria Name">Name</div>`,
-  `<div role="tooltip" id="title" title="Title"></div>`,
-];
-
-const violations = [
-  `<div role="tooltip" id="empty"></div>`,
-  `<div role="tooltip" id="alempty" aria-label=""></div>`,
-  `<div
-      role="tooltip"
-      id="albmissing"
-      aria-labelledby="nonexistent"
-    ></div>`,
-  `<div>
-    <div role="tooltip" id="albempty" aria-labelledby="emptydiv"></div>
-    <div id="emptydiv"></div>
-  </div>`,
-];
-
-describe("aria-tooltip-name", async function() {
-  for (const markup of passes) {
-    const el = await fixture(html`${markup}`);
-    it(el.outerHTML, async () => {
-      const results = (await scanner.scan(el)).map(({ text, url }) => {
-        return { text, url };
-      });
-
-      expect(results).to.be.empty;
-    });
-  }
-
-  for await (const markup of violations) {
-    const el = await fixture(html`${markup}`);
-    it(el.outerHTML, async () => {
-      const results = (await scanner.scan(el)).map(({ text, url }) => {
-        return { text, url };
-      });
-
-      expect(results).to.eql([
-        {
-          text: "ARIA tooltip must have an accessible name",
-          url: "https://dequeuniversity.com/rules/axe/4.4/aria-tooltip-name?application=RuleDescription",
-        },
-      ]);
-    });
-  }
-});
-=======
 import { fixture, expect } from "@open-wc/testing";
 import { Scanner } from "../src/scanner";
 import { ariaTooltipName } from "../src/rules/aria-tooltip-name";
@@ -91,8 +32,6 @@ describe("aria-tooltip-name", async function () {
   for (const markup of passes) {
     const el = await fixture(markup);
     it(el.outerHTML, async function () {
-      console.log(markup, el.outerHTML);
-      this.skip();
       const results = (await scanner.scan(el)).map(({ text, url }) => {
         return { text, url };
       });
@@ -104,8 +43,6 @@ describe("aria-tooltip-name", async function () {
   for (const markup of violations) {
     const el = await fixture(markup);
     it(el.outerHTML, async function () {
-      console.log(markup, el.outerHTML);
-      this.skip();
       const results = (await scanner.scan(el)).map(({ text, url }) => {
         return { text, url };
       });
