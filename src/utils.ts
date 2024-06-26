51c8089ba33ef5a64423e2e69615824204665d37
Kristján Oddsson <koddsson@gmail.com>
2024-06-26 16:51:34 +0200

    remove uneccesary todo

--- src/utils.ts
+++ src/utils.ts
@@ -6,8 +6,6 @@ export function isVisible(el: HTMLElement): boolean {
 /**
  * Given a element, make sure that it's `aria-labelledby` has a value and it's
  * value maps to a element in the DOM that has valid text
- *
- * @TODO: Make the `document` argument a required argument
  **/
 export function labelledByIsValid(el: Element): boolean {
   const id = el.getAttribute("aria-labelledby");
